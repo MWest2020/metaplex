1b6444910440cebd54a655eea3615bda7da5c42b
MWest2020 <mwesterweel@hotmail.com>
2021-09-01 00:55:58 +0200

    err connection.tsx line 556 conditional

--- js/packages/common/src/contexts/connection.tsx
+++ js/packages/common/src/contexts/connection.tsx
@@ -553,7 +553,7 @@ export async function sendSignedTransaction({
     slot = confirmation?.slot || 0;
   } catch (err) {
     console.error('Timeout Error caught', err);
-    if (err.timeout) {
+    if ((err as any).timeout) {
       throw new Error('Timed out awaiting confirmation on transaction');
     }
     let simulateResult: SimulatedTransactionResponse | null = null;
